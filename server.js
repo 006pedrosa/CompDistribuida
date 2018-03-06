54e78858fb4b8462191d16146ac7f12d16f2ebc7
jorge <jorge mauro goncalves>
2018-03-06 13:48:01 -0300

    mudança server

--- server.js
+++ server.js
@@ -134,4 +134,3 @@ app.use('/pagamentoapi', router);
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
 });
-console.log("Iniciando api pela porta "+ port); 
